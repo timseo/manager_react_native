72574450af70e1541fc943e6e0dd15348165fcc1
tim seo <gdurtyfive@gmail.com>
2017-06-21 11:12:58 -0700

    add button

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
+import EmployeeList from './components/EmployeeList'
 
 const RouterComponent = () => {
   return (
-    <Router>
-      <Scene key="login" component={LoginForm} title="Please Login" />
+    <Router sceneStyle={{ paddingTop: 63 }}>
+
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Please Login" initial />
+      </Scene>
+
+      <Scene key="main">
+        <Scene
+          onRight={() => console.log('right!!!!!!')}
+          rightTitle="Add"
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees"
+        />
+      </Scene>
+
     </Router>
   )
 }
